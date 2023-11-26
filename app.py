a55e7e840c32f5a47604da1b6324875fb0a68842
Angel Valladares <arivalladares2.0@gmail.com>
2023-11-26 14:43:37 -0600

    first stable version.

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cargar el modelo al iniciar la aplicación
+modelo = joblib.load('risk.pkl')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    # Obtener datos de la solicitud
+    datos_json = request.json
+    # Convertir los datos en un DataFrame
+    datos_para_prediccion = pd.DataFrame([datos_json])
+    # Realizar la predicción usando el modelo cargado
+    resultado_prediccion = modelo.predict(datos_para_prediccion)
+    # Devolver el resultado como JSON
+    return jsonify({'prediccion': resultado_prediccion.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
