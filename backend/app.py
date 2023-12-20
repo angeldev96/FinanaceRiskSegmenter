33169a00db93d6653d3b9f6aea9597c820c2310e
Angel Valladares <arivalladares2.0@gmail.com>
2023-12-20 00:59:13 -0600

    Update model loading and endpoint name
    This commit updates the model loading code in the backend app and renames the endpoint from '/predecir' to '/predict'.

--- backend/app.py
+++ backend/app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the model during application startup
-modelo = joblib.load('models/risk.pkl')
+model = joblib.load('models/risk.pkl')
 
-@app.route('/predecir', methods=['POST'])
-def predecir():
+@app.route('/predict', methods=['POST'])
+def predict():
     """
     Endpoint for making predictions based on input data.
 
@@ -20,19 +20,19 @@ def predecir():
     """
     try:
         # Get data from the request
-        datos_json = request.json
+        json_data = request.json
 
         # Validate input data
-        validate_input(datos_json)
+        validate_input(json_data)
 
         # Convert the data into a DataFrame
-        datos_para_prediccion = pd.DataFrame([datos_json])
+        prediction_data = pd.DataFrame([json_data])
 
         # Make prediction using the loaded model
-        resultado_prediccion = modelo.predict(datos_para_prediccion)
+        prediction_result = model.predict(prediction_data)
 
         # Return the result as JSON
-        return jsonify({'prediccion': resultado_prediccion.tolist()})
+        return jsonify({'prediction': prediction_result.tolist()})
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         return jsonify({'error': 'An error occurred'}), 500
